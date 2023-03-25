69d42a8e0244e720e3e0db424bb5528001594209
arwakheir <arwa.kheir@tkh.edu.eg>
2023-03-25 17:12:46 +0200

    hit

---
+++ Assets/scripts/Hit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    Vector2 startPos;
+     private void Start() {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+
+       
+        if (other.CompareTag("Hit"))
+        {
+            Die();
+       } 
+    }
+void Die()
+{
+    Respawn();
+}
+
+void Respawn()
+{
+    transform.position = startPos;
+}
+}
