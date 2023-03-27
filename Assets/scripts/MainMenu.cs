21b73a78c86dae13f48d38bc71d61b629946973f
arwakheir <arwa.kheir@tkh.edu.eg>
2023-03-27 03:09:17 +0200

    update

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -17,5 +17,10 @@ public class MainMenu : MonoBehaviour
         Debug.Log("QUIT");
         Application.Quit();
     }
+
+    public void Back()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
     
 }
